87315fd013fa0ce80de7bffae8e2d958b1e65a28
dlscoccia <2054@holbertonschool.com>
2021-06-24 17:06:39 -0500

    fixing spaces

--- 0x0C-Javascript_advanced/14-wikipedia.js
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -9,10 +9,10 @@ function createElement(data) {
 function queryWikipedia(callback) {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState == 4 && this.status == 200) {
         callback(xhttp.response.query.pages["21721040"].extract);
-      }
-    };
+    }
+};
 
     xhttp.open("GET", "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*", true);
     xhttp.responseType = "json";
