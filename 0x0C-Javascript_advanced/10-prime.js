28dcd160a164c984f87b5156420be01e91a87f1b
dlscoccia <2054@holbertonschool.com>
2021-06-24 15:45:34 -0500

    Adding task 10; prime numbers 100 times

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,23 @@
+function countPrimeNumbers() {
+    let totalPrimes = 0
+    let counter = 0
+    for (let i = 2; i <= 100; i++){
+        for(let j = 2; j <= i; j++){
+            if (i % j === 0) {
+                counter += 1
+            }
+        }
+        if (counter == 1) {
+            totalPrimes += 1
+        }
+        counter = 0
+    }
+    return totalPrimes
+}
+
+const t0 = performance.now();
+for (let i =0; i < 100; i++){
+    countPrimeNumbers();
+}
+const t1 = performance.now();
+console.log(`Execution time of calculating prime numbers 100 times was ${t1 - t0} milliseconds.`);
